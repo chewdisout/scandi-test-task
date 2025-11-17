abaf685a342289257a96b0e0c0381823f669f5e4
chewdisout <chewdisout@gmail.com>
2025-11-17 16:08:18 +0000

    CORS

--- backend/src/Bootstrap/App.php
+++ backend/src/Bootstrap/App.php
@@ -12,6 +12,15 @@ class App
 {
     public function run(): void
     {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization');
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            http_response_code(200);
+            exit;
+        }
+
         $dotenvPath = __DIR__ . '/../../';
         if (file_exists($dotenvPath . '.env')) {
             Dotenv::createImmutable($dotenvPath)->load();
