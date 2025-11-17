417cf5c14690e60fb0a71076b1310d3bc7e1f652
chewdisout <chewdisout@gmail.com>
2025-11-17 16:02:55 +0000

    Fixed FE

--- frontend/src/apollo.ts
+++ frontend/src/apollo.ts
@@ -1,6 +1,10 @@
+// apollo.ts
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
+const uri = 'https://scandi-test-task-be.onrender.com/graphql';
+
 export const client = new ApolloClient({
-  link: new HttpLink({ uri: '/graphql' }),
+  link: new HttpLink({ uri }),
   cache: new InMemoryCache(),
 });
+
